e8c196369c6414a45440a034251cb566f4a7368f
James McIntyre <james.checks.his.email@gmail.com>
2020-11-23 22:45:16 -0500

    switching to gh pages from aws

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ const desktopConstraints = {
   // height: 720,
   facingMode: 'user'
 };
-const gobbleURL = 'https://s3-us-west-1.amazonaws.com/theturkeyapp/gobblegobble.m4a';
+// const gobbleURL = 'https://s3-us-west-1.amazonaws.com/theturkeyapp/gobblegobble.m4a';
+const gobbleURL = 'https://turkeyornah.com/gobblegobble.m4a';
 var isMobile = false; //initiate as false
 // device detection
 if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
